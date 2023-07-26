9b4a5f9bf56e16b86741c277c3a3cc36ca3f2799
alessandrostorchi92 <storchi.alle@gmail.com>
2023-07-26 17:46:12 +0200

    inizio Milestone 2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,215 @@
+// Seguendo il brief allegato, creare un mini clone di Whatsapp.
+
+// Milestone 1
+// ● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
+// dall’interlocutore (bianco) assegnando due classi CSS diverse
+// ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
+// nome e immagine di ogni contatto
+
+// Milestone 2
+// ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+// messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// ● Click sul contatto mostra la conversazione del contatto cliccato
+
+// Milestone 3
+// ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+// “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+// un “ok” come risposta, che apparirà dopo 1 secondo.
+
+// Milestone 4
+// ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+// contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+// “mar” rimangono solo Marco e Martina)
+
 "use script"
 
+// Procedimento Logico:
+
+// - Milestone 1:
+// 1) Inserisci all'interno del return della funzione data gli array di oggetti
+// 2) Cancello gli elementi statici della lista contatti tranne un contatto per aggiungerli dinamicamente mediante la direttiva v-for
+
+// - Milestone 2:
+// 1) Una volta stampata la lista degli utenti, al click su un utente devo mostrare il suo avatar sulla barra in alto e i suoi messaggi sulla schermata di destra
+// 2) Per fare ciò utilizzo l'oggetto dell'utente cliccato e lo salvo in una variabile (currentContact) che inserisco nel return della funzione data()
+
 Vue.createApp({
-	data() {
+    data() {
         return {
-            
+            currentContact: null,
+            contactsList: [
+                {
+                    name: 'Michele',
+                    avatar: '_1',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '202/01/10 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Fabio',
+                    avatar: '_2',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20-03-2020T16:30:00',
+                            message: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            message: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Samuele',
+                    avatar: '_3',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            message: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            message: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            message: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Alessandro',
+                    avatar: '_4',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: "Vincenzo",
+                    avatar: "_5",
+                    messages: [
+                        {
+                            date: "10/01/2020 15:30:55",
+                            message: "Hai portato a spasso il cane?",
+                            status: "sent",
+                        },
+                        {
+                            date: "10/01/2020 15:50:00",
+                            message: "Ricordati di dargli da mangiare",
+                            status: "sent",
+                        },
+                        {
+                            date: "10/01/2020 16:15:22",
+                            message: "Tutto fatto!",
+                            status: "received",
+                        },
+                    ],
+                },
+                {
+                    name: "Sara",
+                    avatar: "_6",
+                    messages: [
+                        {
+                            date: "20/03/2020 16:30:00",
+                            message: "Ciao come stai?",
+                            status: "sent",
+                        },
+                        {
+                            date: "20/03/2020 16:30:55",
+                            message: "Bene grazie! Stasera ci vediamo?",
+                            status: "received",
+                        },
+                        {
+                            date: "20/03/2020 16:35:00",
+                            message: "Certo! Vedrai che ci divertiremo.",
+                            status: "sent",
+                        },
+                    ],
+                },
+                {
+                    name: "Umberto",
+                    avatar: "_7",
+                    messages: [
+                        {
+                            date: "28/03/2020 10:10:40",
+                            message: "La Marianna va in campagna",
+                            status: "received",
+                        },
+                        {
+                            date: "28/03/2020 10:20:10",
+                            message: "Sicuro di non aver sbagliato chat?",
+                            status: "sent",
+                        },
+                        {
+                            date: "28/03/2020 16:15:22",
+                            message: "Ah scusa!",
+                            status: "received",
+                        },
+                    ],
+                },
+                {
+                    name: "Davide",
+                    avatar: "_8",
+                    messages: [
+                        {
+                            date: "10/01/2020 15:30:55",
+                            message: "Lo sai che ha aperto una nuova pizzeria?",
+                            status: "sent",
+                        },
+                        {
+                            date: "10/01/2020 15:50:00",
+                            message: "Si, ma preferirei andare al cinema",
+                            status: "received",
+                        },
+                    ],
+                },
+            ]
         }
+    },
+    methods: {
+        onUserClick(singleContact) {
+            this.currentContact = singleContact
+        }
+    },
+    beforeMount() {
+        this.currentContact = this.contactsList[0]
     }
 }).mount("#app")
