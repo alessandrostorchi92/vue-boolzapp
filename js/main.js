368bbd13cd8e9f8ce2f050ad511d03258b21ded5
alessandrostorchi92 <storchi.alle@gmail.com>
2023-07-27 00:59:18 +0200

    Scroll automaticodei messaggi

--- js/main.js
+++ js/main.js
@@ -4,23 +4,23 @@
 // ● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
 // dall’interlocutore (bianco) assegnando due classi CSS diverse
 // ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
-// nome e immagine di ogni contatto
+// nome e immagine di ogni contatto.
 
 // Milestone 2
 // ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
-// messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// messaggi relativi al contatto attivo all’interno del pannello della conversazione;
 // ● Click sul contatto mostra la conversazione del contatto cliccato
 
 // Milestone 3
 // ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
-// “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// “enter” il testo viene aggiunto al thread sopra, come messaggio verde;
 // ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
 // un “ok” come risposta, che apparirà dopo 1 secondo.
 
 // Milestone 4
 // ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
 // contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
-// “mar” rimangono solo Marco e Martina)
+// “mar” rimangono solo Marco e Martina).
 
 "use script"
 
@@ -227,19 +227,25 @@ Vue.createApp({
             // Leggo il testo che l'utente ha scritto
             console.log(this.newMessageText);
 
+            
             // Rappresenta l'oggetto che identifica l'utente attivo, che si attiva al click sulla lista dei contatti 
             this.currentContact
-
+            
             // Array dei messaggi della chat dell'utente attualmente attivo
             this.currentContact.messages
-
+            
             // Per aggiungere il nuovo messaggio uso .push
             this.currentContact.messages.push({
                 date: new Date().toLocaleDateString() + "" + new Date().toLocaleTimeString(),
                 message: this.newMessageText,
                 status: "sent",
             })
+            
+            // Per accedere ad un elemento html tramite Vue posso usare this.$refs dopo aver aggiunto l'attributo ref all'elemento html. Appena Vue finisce di aggiornare l'html, eseguo subito il codice sottostante mediante questo metodo nativo di Vuejs
 
+            this.$nextTick(() => {
+                this.$refs.msgContainer.scrollTop = this.$refs.msgContainer.scrollHeight;
+            });
         },
     },
     beforeMount() {
